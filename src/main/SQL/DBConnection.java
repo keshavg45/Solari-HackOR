5e955d062e7189a21780880d8cf6a561f5754046
rafa3l389@gmail.com <buhtiG389@>
2021-03-26 21:27:57 -0700

    Database Update: Created DatabaseInitializer Class for the local hackor.db

--- src/main/SQL/DBConnection.java
+++ src/main/SQL/DBConnection.java
@@ -8,8 +8,7 @@ public class DBConnection {
         Connection con = null;
         try {
             Class.forName("org.sqlite.JDBC");
-            con = DriverManager.getConnection("jdbc:sqlite:/Users/yajasmalhotra/Documents/UBC/Year 2/CPSC 210/" +
-                    "Projects/HackOR-Project/hackor.db"); // connect to database
+            con = DriverManager.getConnection("jdbc:sqlite:hackor.db"); // connect to database
             System.out.println("Connected to database");
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println(e+"");
--- src/main/SQL/VolunteerView.java
+++ src/main/SQL/VolunteerView.java
@@ -1,29 +1,25 @@
 package SQL;
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class VolunteerView {
-    public static Connection connect() {
-        Connection con = null;
+    public static void main (String []args){
+        Connection myConnection = DBConnection.connect();
         try {
-            Class.forName("org.sqlite.JDBC");
-            con = DriverManager.getConnection("jdbc:sqlite:/Users/yajasmalhotra/Documents/UBC/Year 2/CPSC 210/" +
-                    "Projects/HackOR-Project/hackor.db"); // connect to database
-            System.out.println("Connected to database");
-
-            Statement myStatement = con.createStatement();
-            String sql = "SELECT * FROM Recruiter";
+            Statement myStatement = myConnection.createStatement();
+            String sql = "SELECT * FROM Volunteer";
             ResultSet rs = myStatement.executeQuery(sql);
 
-            while(rs.next()){
-                System.out.print("Recruiter Name: " + rs.getString(1) + " Company Name: " +
-                        rs.getString(2));
+            System.out.println("Volunteer Bios:");
+            while (rs.next()){
+                System.out.println("Name: " + rs.getString(1));
+                System.out.println("Age: " + rs.getString(2));
             }
-
-
-        } catch (ClassNotFoundException | SQLException e) {
+        } catch (SQLException e){
             System.out.println(e + "");
         }
-
-        return con;
     }
 }
