4b71c50759e9d37f43905a2a5a39e62a80dca7b8
keshavg45 <keshavg45@gmail.com>
2021-03-26 20:21:03 -0700

    Linked VolunteerPage and RecruiterPage with HomePage

--- src/main/ui/MainGUI.java
+++ src/main/ui/MainGUI.java
@@ -1,6 +1,9 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainGUI extends Application {
@@ -11,6 +14,11 @@ public class MainGUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Parent root = FXMLLoader.load(getClass().getResource("homepage/homepage.fxml"));
+//        primaryStage.getIcons().add(new Image(MainGUI.class.getResourceAsStream("logo.jpg")));
+        primaryStage.setTitle("Solari");
+        Scene scene = new Scene(root, 900, 700);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
