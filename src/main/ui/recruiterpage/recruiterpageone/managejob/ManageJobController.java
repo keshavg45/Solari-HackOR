7f2c343d4476bcef8f59b31b02c31cb2c86613ef
keshavg45 <keshavg45@gmail.com>
2021-03-28 03:54:39 -0700

    Update: Added button and ListView handlers for ManageJobController

--- src/main/ui/recruiterpage/recruiterpageone/managejob/ManageJobController.java
+++ src/main/ui/recruiterpage/recruiterpageone/managejob/ManageJobController.java
@@ -2,12 +2,14 @@ package ui.recruiterpage.recruiterpageone.managejob;
 
 import SQL.DBConnection;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 import ui.recruiterpage.recruiterpageone.RecruiterPageOneController;
 
@@ -22,14 +24,74 @@ import java.util.List;
 public class ManageJobController {
 
     public Button back;
+    public Button viewVolunteers;
+    public Button closePosting;
+    public Button addVolunteer;
+
     public Label companyLabel;
 
+    public ListView<String> jobPostings;
+    public ListView<String> volunteersAvailable;
+
+    public int jobSelectedIndex;
+    public int volunteerSelectedIndex;
+
     Connection myConnection = DBConnection.connect();
+
+    @FXML
+    private void initialize() {
+        jobPostings.getItems().add("Job 1");
+        jobPostings.getItems().add("Job 2");
+        jobPostings.getItems().add("Job 3");
+    }
 
     public void buttonClick(ActionEvent event) throws IOException {
         if (event.getSource() == back) {
             backButtonClicked(event);
+        } else if (event.getSource() == viewVolunteers) {
+            showVolunteers();
+        } else if (event.getSource() == closePosting) {
+            closeJobPosting();
+        } else if (event.getSource() == addVolunteer) {
+            addaVolunteer();
         }
+    }
+
+    public void showVolunteers() {
+        if (jobSelectedIndex == 0) {
+            volunteersAvailable.getItems().clear();
+            volunteersAvailable.getItems().add("Mikey");
+            volunteersAvailable.getItems().add("Dwight");
+            volunteersAvailable.getItems().add("Jim");
+        } else if (jobSelectedIndex == 1) {
+            volunteersAvailable.getItems().clear();
+            volunteersAvailable.getItems().add("Angela");
+            volunteersAvailable.getItems().add("Pam");
+            volunteersAvailable.getItems().add("Andy");
+        }  else if (jobSelectedIndex == 2) {
+            volunteersAvailable.getItems().clear();
+            volunteersAvailable.getItems().add("Stanley");
+            volunteersAvailable.getItems().add("Micheal");
+            volunteersAvailable.getItems().add("Creed");
+        }
+    }
+
+    public void closeJobPosting() {
+
+    }
+
+    public void addaVolunteer() {
+
+    }
+
+    public void selectJob() {
+        jobSelectedIndex = jobPostings.getSelectionModel().getSelectedIndex();
+        System.out.println("Manage Job Index:" + jobSelectedIndex);
+    }
+
+    public void selectVolunteer() {
+        volunteerSelectedIndex = volunteersAvailable.getSelectionModel().getSelectedIndex();
+        System.out.println("Volunteers Index:" + volunteerSelectedIndex);
     }
 
     public void backButtonClicked(ActionEvent event) throws IOException {
